e65beaa9682c4be1503cdcb8d7b6be20db6b8cc6
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-04-26 10:21:18 -0700

    Created Wrapper, Content, and LogoImg Styled Components.

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background: var(--darkGray);
+  padding: 0 20px;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: var(--maxWidth);
+  padding: 20px 0;
+  margin: 0 auto;
+`;
+
+export const LogoImg = styled.img`
+  width: 75px;
+  @media screen and (max-width: 500px) {
+    width: 50px;
+  }
+`;
