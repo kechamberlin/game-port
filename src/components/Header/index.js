b5685ab3e2077a460c4da3be4828bc959ab4c473
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-04-26 10:22:12 -0700

    Created a Header Component that displays the App's Logo.

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Wrapper, Content, LogoImg } from './Header.styles';
+import GamePortLogo from '../../images/joystick-logo.svg';
+
+export default function Header() {
+  return (
+    <Wrapper>
+      <Content>
+        <LogoImg src={GamePortLogo} alt="game-port-logo" />
+      </Content>
+    </Wrapper>
+  );
+}
