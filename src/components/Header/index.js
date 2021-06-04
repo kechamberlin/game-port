5dba2c583b8a51975c10eda6771fdfb039d75e3f
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-06-04 09:20:03 -0700

    Imported link from react router.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Wrapper, Content, LogoImg, Text } from './Header.styles';
 import GamePortLogo from '../../images/joystick-logo.svg';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
