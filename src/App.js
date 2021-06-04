1fc57ab7ed41de0ff03c0d9a1e033123c8b0e284
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-06-04 09:25:34 -0700

    Imported react router, game component, home component, and not found component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Game from './components/Game';
 import Header from './components/Header';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
 import { GlobalStyle } from './GlobalStyle';
 
 function App() {
